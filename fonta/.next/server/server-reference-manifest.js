3897c10c2e87dfd35bffe0981f43d666b301e20c
elduverx <your.email@example.com>
2025-05-09 15:47:22 +0200

    ramon web test

--- fonta/.next/server/server-reference-manifest.js
+++ fonta/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"9NcB+yUl8SK0UyeTXykdEn2diHd+2HLup0IyXpylPM0=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"td4NzzpyvSGUVy3JBFHRwb+Wc/EJ7oSr+tXuCokoRkU=\"\n}"
--- fonta/next.config.js
+++ fonta/next.config.js
@@ -14,6 +14,13 @@ const nextConfig = {
       },
     ],
   },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': require('path').resolve(__dirname, 'src'),
+    };
+    return config;
+  },
 }
 
 module.exports = nextConfig 
